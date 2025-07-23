506f1e80381ece4d8dbd418cd7dfe954dbf40d64
ParasKanchan <paraskanchan438@gmail.com>
2025-07-24 01:20:54 +0545

    Update script.js

--- SimonGame/script.js
+++ SimonGame/script.js
@@ -32,7 +32,7 @@ function levelUp(){
     level++;
     h2.innerText=`Level ${level}`;
 
-    let randIdx = Math.floor(Math.random()*3);
+    let randIdx = Math.floor(Math.random()*4);
     let randColor = btns[randIdx];
     let randbtn = document.querySelector(`.${randColor}`);
     
